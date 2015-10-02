9a8caf5aec5cbc57ab2e45332af3723942e4df8b
robert berry <berrydigital@gmail.com>
2015-10-01 23:56:40 -0700

    do not apply 1st changes; include ste abbreviation

--- lib/stellar_cartography/symbols.rb
+++ lib/stellar_cartography/symbols.rb
@@ -1,6 +1,6 @@
 module StellarCartography
   
-  ORDINAL_NUMBERS = {"1st" => "first", "2nd" => "second", "3rd" => "third", "4th" => "fourth", "5th" => "fifth", "6th" => "sixth", "7th" => "seventh", "8th" => "eigth", "9th" => "ninth", "10th" => "tenth", "11th" => "eleventh", "12th" => "twelfth", "13th" => "thirteenth"}
+  ORDINAL_NUMBERS = {} #{"1st" => "first", "2nd" => "second", "3rd" => "third", "4th" => "fourth", "5th" => "fifth", "6th" => "sixth", "7th" => "seventh", "8th" => "eigth", "9th" => "ninth", "10th" => "tenth", "11th" => "eleventh", "12th" => "twelfth", "13th" => "thirteenth"}
   
   ORDINAL_DIRECTIONS = {"s" => "south", "n" => "north", "e" => "east", "w" => "west", "se" => "southeast", "sw" => "southwest", "ne" => "northeast", "nw" => "northwest"} 
   
